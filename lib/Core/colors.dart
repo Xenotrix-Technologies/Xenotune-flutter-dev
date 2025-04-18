b6942824be241869e99860510c103775ac12d9b8
H4r1 <harigovindappu4926@gmail.com>
2025-04-18 15:40:15 +0530

    initial commit

---
+++ lib/Core/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kblack = Colors.black;
+const kwhite = Colors.white;
+const kPrimaryBlue = Color(0xFF0600C2);
+const kPrimaryPurple = Color(0xFFC40DAC);
