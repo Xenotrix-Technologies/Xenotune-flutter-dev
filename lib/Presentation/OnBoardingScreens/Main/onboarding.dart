6870b5e2b31139b3b6822f4f3691ef08789225f1
H4r1 <harigovindappu4926@gmail.com>
2025-04-24 18:09:06 +0530

    new onboarding screens

---
+++ lib/Presentation/OnBoardingScreens/Main/onboarding.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:xenotune_flutter_dev/Presentation/OnBoardingScreens/Screens/username.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  final PageController _pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          children: [UsernamePage()],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Presentation/OnBoardingScreens/Screens/username.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:xenotune_flutter_dev/Core/colors.dart';
+import 'package:xenotune_flutter_dev/Core/google_fonts.dart';
+import 'package:xenotune_flutter_dev/Core/sized_box.dart';
+import 'package:xenotune_flutter_dev/Presentation/Widgets/Gradient%20Header/gradient_header.dart';
+
+class UsernamePage extends StatelessWidget {
+  const UsernamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ktransparent,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [kPrimaryBlueDark, kblack, kPrimaryPurpleDark],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+            GradientHeader(
+              bottom: 0,
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: kMqHeight(context) * 0.05,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: kMqWidth(context) * 0.07),
+                      child: Text(
+                        'What do we call you?',
+                        style: inter(color: kwhite),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: kMqWidth(context) * 0.07,
+                      ),
+                      child: TextField(
+                        showCursor: true,
+                        autofocus: true,
+                        maxLength: 30,
+                        cursorColor: kwhite,
+
+                        decoration: InputDecoration(
+                          counterText: ' ',
+                          focusColor: kwhite,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: kwhite),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: kwhite),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Navigator.push(
+                          //   context,
+                          //   PageRouteBuilder(
+                          //     transitionDuration: Duration(milliseconds: 500),
+                          //     pageBuilder:
+                          //         (context, animation, secondaryAnimation) =>
+                          //             RelaxPage(),
+                          //     transitionsBuilder: (
+                          //       context,
+                          //       animation,
+                          //       secondaryAnimation,
+                          //       child,
+                          //     ) {
+                          //       return FadeTransition(
+                          //         opacity: animation,
+                          //         child: child,
+                          //       );
+                          //     },
+                          //   ),
+                          // );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                        ),
+                        child: Ink(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: kMqWidth(context) * 0.1,
+                            vertical: 12,
+                          ),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [kPrimaryPurple, kblack, kPrimaryBlue],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          child: Text('Continue', style: inter(color: kwhite)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                GradientText(
+                  textAlign: TextAlign.center,
+                  gradientDirection: GradientDirection.ltr,
+
+                  style: poppins(fontSize: 22, height: 2.5),
+
+                  'Music doesn\'t just fill\nthe silence — it rewrites it.\nOne note can lift a heart, \nslow a breath,\nor calm a storm within.',
+                  colors: [kPrimaryPurple, kPrimaryBlue],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
