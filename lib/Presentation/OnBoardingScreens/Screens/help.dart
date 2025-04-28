b073bcc0ec6ecf786dfb80d823e87e21d4782559
H4r1 <harigovindappu4926@gmail.com>
2025-04-28 13:32:12 +0530

    helps to  page ui completed

---
+++ lib/Presentation/OnBoardingScreens/Screens/help.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:xenotune_flutter_dev/Core/colors.dart';
+import 'package:xenotune_flutter_dev/Core/google_fonts.dart';
+import 'package:xenotune_flutter_dev/Core/sized_box.dart';
+import 'package:xenotune_flutter_dev/Presentation/Widgets/Gradient%20Header/gradient_header.dart';
+
+class HelpsToPage extends StatefulWidget {
+  final Function()? onBack;
+  final void onTap;
+  const HelpsToPage({super.key, this.onBack, this.onTap});
+
+  @override
+  State<HelpsToPage> createState() => _HelpsToPageState();
+}
+
+class _HelpsToPageState extends State<HelpsToPage> {
+  String? helpsTo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ktransparent,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [kPrimaryBlueDark, kblack, kPrimaryPurpleDark],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+            GradientHeader(
+              bottom: 0,
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: kMqHeight(context) * 0.02,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: kMqWidth(context) * 0.07),
+                  child: Text(
+                    'What do you want the music\nto help you do?',
+                    style: inter(color: kwhite),
+                  ),
+                ),
+                Center(
+                  child: InkWell(
+                    onTap: () {
+                      widget.onTap;
+                      setState(() {
+                        helpsTo = 'Concentrate';
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: kMqHeight(context) * 0.02,
+                      ),
+                      height: kMqHeight(context) * 0.10,
+                      width: kMqWidth(context) * 0.8,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(23),
+                        gradient: LinearGradient(
+                          colors: [kConcentrateDart, kConcentrateLight],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Concentrate',
+                          style: poppins(color: kwhite, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: InkWell(
+                    onTap: () {
+                      widget.onTap;
+                      setState(() {
+                        helpsTo = 'Calm Down';
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: kMqHeight(context) * 0.02,
+                      ),
+                      height: kMqHeight(context) * 0.10,
+                      width: kMqWidth(context) * 0.8,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(23),
+                        gradient: LinearGradient(
+                          colors: [kCalmDownLight, kCalmDownDart],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Calm Down',
+                          style: poppins(color: kwhite, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: InkWell(
+                    onTap: () {
+                      widget.onTap;
+                      setState(() {
+                        helpsTo = 'Fall Asleep';
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: kMqHeight(context) * 0.02,
+                      ),
+                      height: kMqHeight(context) * 0.10,
+                      width: kMqWidth(context) * 0.8,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(23),
+                        gradient: LinearGradient(
+                          colors: [kFallAsleepDart, kFallAsleepLight],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Fall Asleep',
+                          style: poppins(color: kwhite, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: InkWell(
+                    onTap: () {
+                      widget.onTap;
+                      setState(() {
+                        helpsTo = 'Get Inspired';
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: kMqHeight(context) * 0.02,
+                      ),
+                      height: kMqHeight(context) * 0.10,
+                      width: kMqWidth(context) * 0.8,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(23),
+                        gradient: LinearGradient(
+                          colors: [kGetInspiredLight, kGetInspiredDart],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Get Inspired',
+                          style: poppins(color: kGrey, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              top: kMqHeight(context) * 0.019,
+              left: kMqWidth(context) * 0.019,
+              child: IconButton(
+                onPressed: widget.onBack,
+                icon: Icon(Icons.arrow_back_ios_new, color: kPrimaryPurple),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
