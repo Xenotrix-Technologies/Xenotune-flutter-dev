e37bf3faceb240de386fccba9a00238addf8ac80
H4r1 <harigovindappu4926@gmail.com>
2025-04-29 13:55:48 +0530

    new loading Screen

--- lib/Presentation/Loading/loading.dart
+++ lib/Presentation/Loading/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xenotune_flutter_dev/Core/colors.dart';
+import 'package:xenotune_flutter_dev/Core/google_fonts.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
@@ -16,6 +17,13 @@ class LoadingScreen extends StatelessWidget {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
               ),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Loading Text\nBased on user response',
+              textAlign: TextAlign.center,
+              style: lexanGiga(color: kwhite),
             ),
           ),
         ],
