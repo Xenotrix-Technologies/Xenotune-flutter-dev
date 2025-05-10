05164430103fe6b266d1c581fbbad97191cf0920
H4r1 <harigovindappu4926@gmail.com>
2025-05-10 13:22:03 +0530

    new sounds page created

---
+++ lib/Presentation/Home/Screens/ultimate_sound.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:xenotune_flutter_dev/Core/colors.dart';
+import 'package:xenotune_flutter_dev/Core/google_fonts.dart';
+import 'package:xenotune_flutter_dev/Core/sized_box.dart';
+import 'package:xenotune_flutter_dev/Presentation/Home/Widgets/sounds.dart';
+
+class SoundsPage extends StatefulWidget {
+  const SoundsPage({super.key});
+
+  @override
+  State<SoundsPage> createState() => _SoundsPageState();
+}
+
+class _SoundsPageState extends State<SoundsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+
+        appBar: AppBar(
+          foregroundColor: kwhite,
+
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_back_ios_new),
+          ),
+          backgroundColor: ktransparent,
+          surfaceTintColor: ktransparent,
+        ),
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [kPrimaryPurpleDark, kblack, kPrimaryBlueDark],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: kMqHeight(context) * 0.05),
+                      child: GradientText(
+                        'Ultra Sound\nCombination',
+                        textAlign: TextAlign.center,
+                        colors: [kPrimaryBlue, kPrimaryPurple],
+                        gradientDirection: GradientDirection.ttb,
+                        style: inter(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  kSizedBoxHeight20,
+                  SoundsViewWidget(soundIcon: Symbols.waves),
+                  kSizedBoxHeight15,
+                  SoundsViewWidget(soundIcon: Symbols.rainy),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Presentation/Home/Widgets/sounds.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:xenotune_flutter_dev/Core/colors.dart';
+import 'package:xenotune_flutter_dev/Core/sized_box.dart';
+
+class SoundsViewWidget extends StatefulWidget {
+  final IconData soundIcon;
+  const SoundsViewWidget({super.key, required this.soundIcon});
+
+  @override
+  State<SoundsViewWidget> createState() => _SoundsViewWidgetState();
+}
+
+class _SoundsViewWidgetState extends State<SoundsViewWidget> {
+  double _value = 0;
+  double _trackHeight = 20;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: kMqWidth(context) * 0.05),
+          child: Icon(widget.soundIcon, color: kwhite, size: 40),
+        ),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _value = 0;
+                });
+              },
+              icon: Icon(
+                _value == 0
+                    ? Symbols.volume_off
+                    : (_value >= 50 ? Symbols.volume_up : Symbols.volume_down),
+                color: kwhite,
+              ),
+            ),
+
+            TweenAnimationBuilder<double>(
+              duration: Duration(milliseconds: 100),
+              tween: Tween<double>(begin: 20, end: _trackHeight),
+              builder: (context, value, child) {
+                return SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    activeTrackColor: kwhite,
+                    thumbColor: kwhite,
+
+                    inactiveTrackColor: kGrey,
+                    trackHeight: value,
+                    thumbShape: SliderComponentShape.noThumb,
+                  ),
+                  child: SizedBox(
+                    width: kMqWidth(context) * 0.65,
+                    child: Slider(
+                      min: 0,
+                      max: 100,
+                      value: _value,
+
+                      onChangeStart: (value) {
+                        setState(() {
+                          _trackHeight = 30;
+                        });
+                      },
+                      onChangeEnd: (value) {
+                        setState(() {
+                          _trackHeight = 20;
+                        });
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          _value = value;
+                        });
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
