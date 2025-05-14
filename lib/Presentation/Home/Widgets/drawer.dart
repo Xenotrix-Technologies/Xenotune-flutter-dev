f6b6c94d488fec94a01c69e79557c013c2f9c210
H4r1 <harigovindappu4926@gmail.com>
2025-05-14 12:54:12 +0530

    drawer ui finished

--- lib/Presentation/Home/Widgets/drawer.dart
+++ lib/Presentation/Home/Widgets/drawer.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
 import 'package:xenotune_flutter_dev/Core/colors.dart';
+import 'package:xenotune_flutter_dev/Core/google_fonts.dart';
+import 'package:xenotune_flutter_dev/Core/sized_box.dart';
+import 'package:xenotune_flutter_dev/Presentation/Login/login_page.dart';
 
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key});
@@ -19,6 +23,80 @@ class DrawerWidget extends StatelessWidget {
           children: [
             DrawerHeader(
               child: CircleAvatar(radius: 50, backgroundColor: kwhite),
+            ),
+            Column(
+              children: [
+                ListTile(
+                  title: Text('Set timer', style: inter(color: kwhite)),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Symbols.arrow_forward_ios, color: kwhite),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Terms & Conditions',
+                    style: inter(color: kwhite),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Symbols.arrow_forward_ios, color: kwhite),
+                  ),
+                ),
+                ListTile(
+                  title: Text('Privacy policy', style: inter(color: kwhite)),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Symbols.arrow_forward_ios, color: kwhite),
+                  ),
+                ),
+                ListTile(
+                  title: Text('Remove ads!', style: inter(color: kwhite)),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Symbols.arrow_forward_ios, color: kwhite),
+                  ),
+                ),
+                ListTile(
+                  title: Text('Upgrade to pro', style: inter(color: kwhite)),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Symbols.arrow_forward_ios, color: kwhite),
+                  ),
+                ),
+              ],
+            ),
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.only(bottom: kMqHeight(context) * 0.02),
+              child: ListTile(
+                title: Text('App version\n1.0.0', style: inter(color: kwhite)),
+                trailing: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        transitionDuration: Duration(milliseconds: 500),
+                        pageBuilder:
+                            (context, animation, secondaryAnimation) =>
+                                LoginPage(),
+                        transitionsBuilder: (
+                          context,
+                          animation,
+                          secondaryAnimation,
+                          child,
+                        ) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('Log In', style: inter(color: kblack)),
+                ),
+              ),
             ),
           ],
         ),
