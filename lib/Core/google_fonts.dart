06d3b6819b461ef205ee16f4646afc8918573bad
H4r1 <harigovindappu4926@gmail.com>
2025-05-15 14:58:56 +0530

    font changed

--- lib/Core/google_fonts.dart
+++ lib/Core/google_fonts.dart
@@ -2,7 +2,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 final lexanGiga = GoogleFonts.lexendGiga;
 final inter = GoogleFonts.inter;
-final kdamThmor = GoogleFonts.kdamThmorPro;
+final kdamThmor = GoogleFonts.josefinSans;
 final outfit = GoogleFonts.outfit;
 final poppins = GoogleFonts.poppins;
 final kosugiMaru = GoogleFonts.kosugiMaru;
