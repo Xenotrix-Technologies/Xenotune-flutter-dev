af024daed2c9840cbfc0a5b4a5f56a2ae77f180f
H4r1 <harigovindappu4926@gmail.com>
2025-05-21 16:55:39 +0530

    added loading animation

--- lib/Presentation/Loading/loading.dart
+++ lib/Presentation/Loading/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart' show RiveAnimation;
 import 'package:xenotune_flutter_dev/Core/colors.dart';
 import 'package:xenotune_flutter_dev/Core/google_fonts.dart';
 import 'package:xenotune_flutter_dev/Presentation/Home/home.dart';
@@ -17,6 +18,16 @@ class LoadingScreen extends StatelessWidget {
                 colors: [kPrimaryPurpleDark, kblack, kPrimaryBlueDark],
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: Opacity(
+              opacity: 0.5,
+              child: RiveAnimation.asset(
+                'assets/animations/honey_comb.riv',
+
+                speedMultiplier: 0.8,
               ),
             ),
           ),
