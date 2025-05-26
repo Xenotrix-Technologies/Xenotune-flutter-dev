cabee8686192cef7646d6fdbcaa8f97888cba4e4
H4r1 <harigovindappu4926@gmail.com>
2025-05-26 17:57:29 +0530

    implimented DJ

---
+++ lib/Domain/Core/Dependency Injection/dependency_injection.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:xenotune_flutter_dev/Domain/Core/Dependency%20Injection/dependency_injection.config.dart';
+
+final getit = GetIt.instance;
+@InjectableInit()
+Future<void> configInjectable() async {
+  getit.init(environment: Environment.prod);
+}
