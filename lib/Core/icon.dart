26d074c12fa628e22df4572f801218ca8d99e824
H4r1 <harigovindappu4926@gmail.com>
2025-06-21 12:18:08 +0530

    new icons

---
+++ lib/Core/icon.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData frog = IconData(0xe900, fontFamily: 'CustomIcon');
+  static const IconData bee = IconData(0xe900, fontFamily: 'CustomIcon');
+}
