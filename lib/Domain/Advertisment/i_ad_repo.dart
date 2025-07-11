d6c0b081b6ac5da3c295fcbb590abb36f0c0591b
H4r1 <harigovindappu4926@gmail.com>
2025-07-11 11:30:45 +0530

    add context field

--- lib/Domain/Advertisment/i_ad_repo.dart
+++ lib/Domain/Advertisment/i_ad_repo.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/widgets.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 abstract class IAdvertisementRepo {
   Future<BannerAd?> showBannerAd();
-  Future<void> showInterstratitialad();
+  Future<void> showInterstratitialad(BuildContext context);
   Future<void> showAppOpenAd();
 }
