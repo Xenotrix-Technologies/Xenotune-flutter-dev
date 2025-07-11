db2cf0e4dd76731eb579b06af64da3bb1a2ea71d
H4r1 <harigovindappu4926@gmail.com>
2025-07-11 11:28:03 +0530

    changed time

--- lib/Infrastructure/Advertisement/show_timed_ad.dart
+++ lib/Infrastructure/Advertisement/show_timed_ad.dart
@@ -1,18 +1,19 @@
 import 'dart:async';
 
+import 'package:flutter/widgets.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 import 'package:xenotune_flutter_dev/Infrastructure/Advertisement/advertisement__repository.dart';
 
 Timer? _adTimer;
 
-void startInterstitialAdTimer() {
+void startInterstitialAdTimer(BuildContext context) {
   _adTimer?.cancel();
 
   InternetConnection().onStatusChange.listen((status) {
     if (status == InternetStatus.connected) {
-      _adTimer = Timer.periodic(Duration(minutes: 15), (timer) {
-        AdvertismentFunctions().showInterstratitialad();
+      _adTimer = Timer.periodic(Duration(minutes: 2), (timer) {
+        AdvertismentFunctions().showInterstratitialad(context);
       });
     }
   });
